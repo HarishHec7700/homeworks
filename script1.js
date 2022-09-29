a000528fb9ea9bf10a6dc7e6dbcdc8a3658ffb48
HarishHec7700 <harish.hariharan7@gmail.com>
2022-09-29 18:14:00 +0530

    Review

--- script1.js
+++ script1.js
@@ -12,3 +12,76 @@ document.getElementById("btn").addEventListener("click",function(){
 	document.getElementById("res").innerHTML="The Present Age is "+age;
 	
 })
+function printRes(e,o){
+	document.getElementById("res-eve").innerHTML="The Sum of Even Numbers are"+e;
+	document.getElementById("res-odd").innerHTML="The Sum of Odd Numbers are"+o;
+
+}
+let sumEle=document.getElementById("reg_sum");
+document.getElementById("reg-btn").addEventListener("click",function(){
+	// console.log("hi");
+	let even=/[02468]/g;
+	let evenSum=0,oddSum=0;
+	let regInp=sumEle.value,inpSli=[...regInp];
+	// console.log(inpSli);
+	inpSli.map((num)=>{
+		if(even.test(num)){
+			num=parseInt(num);
+			evenSum+=num;
+		}else{
+			num=parseInt(num);
+			oddSum+=num;
+		}
+		
+	})
+	printRes(evenSum,oddSum);
+})
+
+
+// let sevRnge=parseInt(prompt("Please enter the range")),sevArray=[],resArr=[],sevStr,indi=0;
+// for(i=1;i<=sevRnge;i++){
+// 	if(i%7==0){
+// 		// sevStr=i.toString();
+// 		resArr.push(i);
+// 		console.log(resArr);
+// 	}
+// }
+
+// for(i=0;i<resArr.length;i++){
+// 	let arr1=resArr[i],
+// 	arr2=resArr[i+1];
+// 	arr1=[...arr1];
+// 	arr2=[...arr2];
+
+// 	if(arr1>arr2){
+// 	}else{
+// 		indi++;
+// 	}
+// 	if(indi==0){
+// 		console.log(resArr[i]);
+// 	}
+// }
+
+let sevRnge=parseInt(prompt("Please enter the range")),sevArray=[],resArr=[],sevStr,indi=0;
+for(i=1;i<=sevRnge;i++){
+	if(i%7==0){
+		// sevStr=i.toString();
+		resArr.push(i);
+		console.log(resArr);
+	}
+}
+
+for(i=0;i<resArr.length;i++){
+	let arr1=resArr[i];
+	console.log(arr1);
+	arr1=arr1.toString();
+	arr1=[...arr1];
+	for(j=0;i<arr1.length;j++){
+		if(arr1[j]<arr1[j+1]){}
+		else
+			indi++;
+		if(indi==0){
+			console.log(resArr[i]);
+		}
+	}	
+}
