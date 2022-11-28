0c3478e01b2eebf58ec1837d826e3fa019a8dedc
HarishHec7700 <harish.hariharan7@gmail.com>
2022-11-28 23:47:02 +0530

    Objects

--- objscript.js
+++ objscript.js
@@ -15,7 +15,13 @@ let arr=[{
     position: "SQL Developer",
     age: 23,
 }]
-let newArr=arr.filter((ele)=>{return(ele.age>23)})
-newArr=arr.sort(function(a,b) {return a.age - b.age});  // (a,b){return (b-a)}
+let newArr=arr.filter((ele)=>{return(ele.age>23)}),condi=0;
+let trueArr=arr.map((ele)=>{
+    if(ele.name=="Bharat Raj"){
+        condi++;
+    }
+});
+console.log(condi);
+// newArr=arr.sort(function(a,b) {return a.age - b.age});  // (a,b){return (b-a)}
 console.log(arr);
 console.log(newArr);
